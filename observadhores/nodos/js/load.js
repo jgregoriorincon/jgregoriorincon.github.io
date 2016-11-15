9ac52cc4f8425dd200ac275703d8f65b6656ea69
jgregoriorincon <jgregoriorincon@gmail.com>
2016-11-15 16:13:54 -0500

    datos

--- observadhores/nodos/js/load.js
+++ observadhores/nodos/js/load.js
@@ -11,60 +11,24 @@ var info, legend, volver;
 // Datos Totales
 var filtroData, filtroLayer;
 var NodosLayer, DptosLayer, MpiosLayer;
-var ObservatoriosLayer;
 
 // Seleccion
 var NodoSeleccionado, DptoSeleccionado, MpioSeleccionado;
-var NodosSur, NodosCentro, NodosCaribe;
+/*var NodosSur, NodosCentro, NodosCaribe;
 var NodoSur, NodoCentro, NodoCaribe;
 
 var NodosSurPutumayo, NodosSurNarino, NodosSurValleCauca, NodosSurCauca;
 var NodosCentroBogota, NodosCentroMeta, NodosCentroBoyaca, NodosCentroSantander, NodosCentroNteSantander;
-var NodosCaribeBolivar, NodosCaribeSucre, NodosCaribeMagdalena, NodosCaribeAtlantico;
+var NodosCaribeBolivar, NodosCaribeSucre, NodosCaribeMagdalena, NodosCaribeAtlantico;*/
 
 var nodoAnterior, dptoAnterior, mpioAnterior, nivelActual;
-
-var observatorioIcon = L.icon({
-    iconUrl: 'css/Map-Marker.png',
-    iconSize: [32, 32],
-    iconAnchor: [22, 31],
-    popupAnchor: [-3, -76]
-});
 
 // Funcion Principal
 $(document).ready(function () {
     "use strict";
 
-    var i, lista = "<option value='all'>Todos</option>";
-    for (i = 0; i < listaNodos.length; i++) {
-        lista += "<option value='" + listaNodos[i] + "'>" + listaNodos[i] + "</option>";
-    }
-    $("#selNodo").html(lista);
-
-    lista = "<option value='all'>Todos</option>";
-    for (i = 0; i < listaSector.length; i++) {
-        lista += "<option value='" + listaSector[i] + "'>" + listaSector[i] + "</option>";
-    }
-    $("#selSector").html(lista);
-
-    lista = "<option value='all'>Todas</option>";
-    for (i = 0; i < listaTematicas.length; i++) {
-        lista += "<option value='" + listaTematicas[i] + "'>" + listaTematicas[i] + "</option>";
-    }
-    $("#selTematica").html(lista);
-
-    lista = "<option value='all'>Todos</option>";
-    for (i = 0; i < listaNivelTerritorial.length; i++) {
-        lista += "<option value='" + listaNivelTerritorial[i] + "'>" + listaNivelTerritorial[i] + "</option>";
-    }
-    $("#selTerritorial").html(lista);
-
-    $("#buscarPalabra").bind("keypress keyup keydown", function (event) {
-        filtrarTodo();
-    });
-
     /* ------------------- MAPA ------------------*/
-    map = L.map('map', {
+    map = L.map('mapNodos', {
         maxZoom: 18,
         minZoom: 5,
         zoomControl: false,
