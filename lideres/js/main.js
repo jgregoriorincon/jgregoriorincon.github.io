efab209c20cd5524b2c2ee7d39e6dde94c5e135a
jgregoriorincon <jgregoriorincon@gmail.com>
2016-12-06 06:45:01 -0500

    cambios

--- lideres/js/main.js
+++ lideres/js/main.js
@@ -74,9 +74,16 @@ function styleDptos(feature) {
 
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    firstDay: true 
   });
 
   $(document).ready(function() {
     $('select').material_select();
   });
+
+  $('.datepicker').pickadate({
+        firstDay: true
+    });
+
+    $(".button-collapse").sideNav();
