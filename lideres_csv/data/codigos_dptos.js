f69776c6453c77c1cb0e69ffaff8e7c5153c105a
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2016-12-16 14:38:55 -0500

    codigos

--- lideres_csv/data/codigos_dptos.js
+++ lideres_csv/data/codigos_dptos.js
@@ -4,60 +4,16 @@ var listaDepartamentos = [
    "NOMBRE": "ANTIOQUIA"
  },
  {
-   "CODIGO": 8,
-   "NOMBRE": "ATLÁNTICO"
- },
- {
-   "CODIGO": 11,
-   "NOMBRE": "BOGOTÁ"
- },
- {
-   "CODIGO": 13,
-   "NOMBRE": "BOLÍVAR"
- },
- {
-   "CODIGO": 15,
-   "NOMBRE": "BOYACÁ"
- },
- {
-   "CODIGO": 17,
-   "NOMBRE": "CALDAS"
- },
- {
    "CODIGO": 18,
-   "NOMBRE": "CAQUETÁ"
+   "NOMBRE": "CAQUETA"
  },
  {
    "CODIGO": 19,
    "NOMBRE": "CAUCA"
  },
  {
-   "CODIGO": 20,
-   "NOMBRE": "CESAR"
- },
- {
    "CODIGO": 23,
-   "NOMBRE": "CÓRDOBA"
- },
- {
-   "CODIGO": 25,
-   "NOMBRE": "CUNDINAMARCA"
- },
- {
-   "CODIGO": 27,
-   "NOMBRE": "CHOCÓ"
- },
- {
-   "CODIGO": 41,
-   "NOMBRE": "HUILA"
- },
- {
-   "CODIGO": 44,
-   "NOMBRE": "LA GUAJIRA"
- },
- {
-   "CODIGO": 47,
-   "NOMBRE": "MAGDALENA"
+   "NOMBRE": "CORDOBA"
  },
  {
    "CODIGO": 50,
@@ -72,63 +28,11 @@ var listaDepartamentos = [
    "NOMBRE": "NORTE DE SANTANDER"
  },
  {
-   "CODIGO": 63,
-   "NOMBRE": "QUINDÍO"
- },
- {
-   "CODIGO": 66,
-   "NOMBRE": "RISARALDA"
- },
- {
-   "CODIGO": 68,
-   "NOMBRE": "SANTANDER"
- },
- {
-   "CODIGO": 70,
-   "NOMBRE": "SUCRE"
- },
- {
    "CODIGO": 73,
    "NOMBRE": "TOLIMA"
  },
  {
-   "CODIGO": 76,
-   "NOMBRE": "VALLE DEL CAUCA"
- },
- {
    "CODIGO": 81,
    "NOMBRE": "ARAUCA"
- },
- {
-   "CODIGO": 85,
-   "NOMBRE": "CASANARE"
- },
- {
-   "CODIGO": 86,
-   "NOMBRE": "PUTUMAYO"
- },
- {
-   "CODIGO": 88,
-   "NOMBRE": "ARCHIPIÉLAGO DE SAN ANDRÉS"
- },
- {
-   "CODIGO": 91,
-   "NOMBRE": "AMAZONAS"
- },
- {
-   "CODIGO": 94,
-   "NOMBRE": "GUAINÍA"
- },
- {
-   "CODIGO": 95,
-   "NOMBRE": "GUAVIARE"
- },
- {
-   "CODIGO": 97,
-   "NOMBRE": "VAUPÉS"
- },
- {
-   "CODIGO": 99,
-   "NOMBRE": "VICHADA"
  }
 ]
